94f93f654fec8ea56be2b97076309c1782e3af51
vivishu <kadiegui1999@gmail.com>
2021-01-29 12:42:53 +0000

    depot git

---
+++ jeudi/monscript.js
@@ -0,0 +1,27 @@
+var donnees=[
+    
+    {region:'Dakar',
+        population:"4million",
+        temperation:"25°C ",
+        densite:"1500hbt/km"
+    },
+    
+    {region:'Thies',
+        population:"2million",
+        temperation:"27°C ",
+        densite:"1200hbt/km",
+    }
+
+];
+
+ 
+ function message() {
+     alert("hello world")
+ };
+
+ 
+ document.getElementById('affiche').innerHTML='bonjour';
+ 
+console.log('affiche.innerHTML');
+console .log(donnees[1].region);
+console.log(donnees[0].densite);
